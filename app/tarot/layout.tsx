dfc14548d6925bc7825833443d863d3116c8b6d7
Itai Ofir <itai510@gmail.com>
2026-05-07 19:44:27 -0700

    change to using navigation buttons

--- app/tarot/layout.tsx
+++ app/tarot/layout.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import Image from "next/image";
+import { Navigation } from "@/components/Navigation";
 
 export default function TarotLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,21 +10,7 @@ export default function TarotLayout({ children }: { children: React.ReactNode })
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <nav className="relative z-50 pt-6 px-6 pb-0">
-        <div className="max-w-7xl mx-auto flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <Link href="/">
-              <Image
-                src="/Logo.svg"
-                alt="Logo"
-                width={48}
-                height={48}
-                priority
-              />
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <Navigation />
       {children}
     </div>
   );
